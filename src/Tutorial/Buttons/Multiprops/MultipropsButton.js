c52f90df8d1bb18cf4801abb8506a55c5eb73f08
Perrine <perrine.brianchon@gmail.com>
2020-04-10 18:16:52 +0200

    set multiple scss themes

--- src/Tutorial/Buttons/Multiprops/MultipropsButton.js
+++ src/Tutorial/Buttons/Multiprops/MultipropsButton.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "./MultipropsButton.module.scss";
 
 /**
- *Now, we gonna melt all that stuff that we learn and make something that will look like a real component. 
+ *Now, we gonna melt all that stuff that we learn and make something that will look like a real component.
  *
  * @visibleName Multi props Button
  */
 
 const MultipropsButton = (props) => {
+  const [styleClass, setstyleClass] = useState(styles.btn);
+
+  useEffect(() => {
+    if (props.theme) {
+      setstyleClass(`${styles.btn}, ${MultipropsButton.themes[props.theme]}`);
+    }
+  });
+
   const style = {
     padding: MultipropsButton.sizes[props.size],
-    color: MultipropsButton.color
+    fontFamily: MultipropsButton.fonts[props.font],
   };
 
   /**
@@ -23,7 +31,7 @@ const MultipropsButton = (props) => {
   };
 
   return (
-    <button style={style} className={styles.btn} onClick={() => doSmthg()}>
+    <button style={style} className={styleClass} onClick={() => doSmthg()}>
       {props.text}
     </button>
   );
@@ -33,25 +41,42 @@ MultipropsButton.propTypes = {
   /** Text content belonging to the button */
   text: PropTypes.string,
 
-  /** Color used for background and border */
-  color: PropTypes.string,
-
   /** What happened when you click */
   onClick: PropTypes.func,
 
   /** The size or your button. */
   size: PropTypes.oneOf(["S", "M", "L"]),
+
+  /** The font-style  or your button. */
+  font: PropTypes.oneOf(["A", "B", "C"]),
+
+  /** Your needed style */
+  theme: PropTypes.oneOf(["primary", "secondary", "tertiary"]),
 };
 
 MultipropsButton.defaultProps = {
   text: "This is me : button !",
   size: "S",
+  font: "A",
 };
 
 MultipropsButton.sizes = {
   S: "0.5em",
   M: "0.8em",
   L: "1.1em",
+};
+
+MultipropsButton.fonts = {
+  A: "cursive",
+  B: "Walter Turncoat",
+  C: "Dokdo",
+  D: "Sue Ellen Francisco",
+};
+
+MultipropsButton.themes = {
+  primary: styles.primary,
+  secondary: styles.secondary,
+  tertiary: styles.tertiary,
 };
 
 export default MultipropsButton;
--- src/Tutorial/Buttons/Multiprops/MultipropsButton.md
+++ src/Tutorial/Buttons/Multiprops/MultipropsButton.md
@@ -1,12 +1,13 @@
 ##### Add fonts
-To maka a component, I will probably will need to add additionnal fonts to my library. Styleguide offers a way to import fonts, stylesheet, favicons and others utils through a very useful tool : the template section of your `styleguide.config.js`. Let's go on Google Fonts and choose one. I will do as usual and then, instead of import into an index html I will add this to my config file: 
+
+To maka a component, I will probably will need to add additionnal fonts to my library. Styleguide offers a way to import fonts, stylesheet, favicons and others utils through a very useful tool : the template section of your `styleguide.config.js`. Let's go on Google Fonts and choose one. I will do as usual and then, instead of import into an index html I will add this to my config file:
 
 ```xml
 template: {
     head: {
       links: [
         {
-          href: "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap",  
+          href: "https://fonts.googleapis.com/css2?family=Amatic+SC:wght@400;700&display=swap",
           rel: "stylesheet",
         },
       ],
@@ -15,18 +16,17 @@ template: {
 
 ```
 
-
 ##### Multiple props
+
 ##### Disabled components
 
-
-```jsx                       
-import Button from "./MultipropsButton"; 
+```jsx
+import Button from "./MultipropsButton";
 
 <div>
-<Button />
-<Button text='Sorry for colors'/>
-<Button size='M' text='Do better next time'/>
-<Button size='L' text='I swear'/>
-</div>
+  <Button theme="primary" />
+  <Button text="Sorry for colors" />
+  <Button size="M" theme="tertiary" font="B" text="Do better next time" />
+  <Button size="L" theme="secondary" text="I swear" />
+</div>;
 ```
